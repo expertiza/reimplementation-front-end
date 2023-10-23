a8369b07b17250b0d96d97573677a7b8c82dac1e
mjoshi6 <mjoshi6@ncsu.edu>
2023-10-23 15:24:45 -0400

    Upated Participant Editor

--- src/pages/Participants/ParticipantEditor.tsx
+++ src/pages/Participants/ParticipantEditor.tsx
@@ -12,13 +12,12 @@ import { HttpMethod } from "utils/httpMethods";
 import * as Yup from "yup";
 import { RootState } from "../../store/store";
 import { IEditor, ROLE } from "../../utils/interfaces";
-import { IUserFormValues, emailOptions, transformUserRequest } from "./participantUtil";
-
+import { IParticipantFormValues, emailOptions, transformParticipantRequest } from "./participantUtil";
 /**
  * @author Ankur Mundra on April, 2023
  */
 
-const initialValues: IUserFormValues = {
+const initialValues: IParticipantFormValues = {
   name: "",
   email: "",
   firstName: "",
@@ -31,9 +30,9 @@ const initialValues: IUserFormValues = {
 const validationSchema = Yup.object({
   name: Yup.string()
     .required("Required")
-    .matches(/^[a-z]+$/, "Username must be in lowercase")
-    .min(3, "Username must be at least 3 characters")
-    .max(20, "Username must be at most 20 characters"),
+    .matches(/^[a-z]+$/, "Name must be in lowercase")
+    .min(3, "Name must be at least 3 characters")
+    .max(20, "Name must be at most 20 characters"),
   email: Yup.string().required("Required").email("Invalid email format"),
   firstName: Yup.string().required("Required").nonNullable(),
   lastName: Yup.string().required("Required").nonNullable(),
@@ -41,50 +40,50 @@ const validationSchema = Yup.object({
   institution_id: Yup.string().required("Required").nonNullable(),
 });
 
-const UserEditor: React.FC<IEditor> = ({ mode }) => {
-  const { data: userResponse, error: userError, sendRequest } = useAPI();
+const ParticipantEditor: React.FC<IEditor> = ({ mode }) => {
+  const { data: participantResponse, error: participantError, sendRequest } = useAPI();
   const auth = useSelector(
     (state: RootState) => state.authentication,
     (prev, next) => prev.isAuthenticated === next.isAuthenticated
   );
-  const { userData, roles, institutions }: any = useLoaderData();
+  const { participantData, roles, institutions }: any = useLoaderData();
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const location = useLocation();
 
   // logged-in user is the parent of the user being created and the institution is the same as the parent's
-  initialValues.parent_id = auth.user.id;
-  initialValues.institution_id = auth.user.institution_id;
+  initialValues.parent_id = auth.participant.id;
+  initialValues.institution_id = auth.participant.institution_id;
 
   // Close the modal if the user is updated successfully and navigate to the users page
   useEffect(() => {
-    if (userResponse && userResponse.status >= 200 && userResponse.status < 300) {
+    if (participantResponse && participantResponse.status >= 200 && participantResponse.status < 300) {
       dispatch(
         alertActions.showAlert({
           variant: "success",
-          message: `User ${userData.name} ${mode}d successfully!`,
+          message: `User ${participantData.name} ${mode}d successfully!`,
         })
       );
       navigate(location.state?.from ? location.state.from : "/users");
     }
-  }, [dispatch, mode, navigate, userData.name, userResponse, location.state?.from]);
+  }, [dispatch, mode, navigate, participantData.name, participantResponse, location.state?.from]);
 
   // Show the error message if the user is not updated successfully
   useEffect(() => {
-    userError && dispatch(alertActions.showAlert({ variant: "danger", message: userError }));
-  }, [userError, dispatch]);
+    participantError && dispatch(alertActions.showAlert({ variant: "danger", message: participantError }));
+  }, [participantError, dispatch]);
 
-  const onSubmit = (values: IUserFormValues, submitProps: FormikHelpers<IUserFormValues>) => {
+  const onSubmit = (values: IParticipantFormValues, submitProps: FormikHelpers<IParticipantFormValues>) => {
     let method: HttpMethod = HttpMethod.POST;
-    let url: string = "/users";
+    let url: string = "/participants";
 
     if (mode === "update") {
-      url = `/users/${values.id}`;
+      url = `/participants/${values.id}`;
       method = HttpMethod.PATCH;
     }
 
     // to be used to display message when user is created
-    userData.name = values.name;
+    participantData.name = values.name;
     sendRequest({
       url: url,
       method: method,
@@ -94,17 +93,17 @@ const UserEditor: React.FC<IEditor> = ({ mode }) => {
     submitProps.setSubmitting(false);
   };
 
-  const handleClose = () => navigate(location.state?.from ? location.state.from : "/users");
+  const handleClose = () => navigate(location.state?.from ? location.state.from : "/participants");
 
   return (
     <Modal size="lg" centered show={true} onHide={handleClose} backdrop="static">
       <Modal.Header closeButton>
-        <Modal.Title>{mode === "update" ? "Update User" : "Create User"}</Modal.Title>
+        <Modal.Title>{mode === "update" ? "Update Participant" : "Create Participant"}</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        {userError && <p className="text-danger">{userError}</p>}
+        {participantError && <p className="text-danger">{participantError}</p>}
         <Formik
-          initialValues={mode === "update" ? userData : initialValues}
+          initialValues={mode === "update" ? participantData : initialValues}
           onSubmit={onSubmit}
           validationSchema={validationSchema}
           validateOnChange={false}
@@ -114,14 +113,14 @@ const UserEditor: React.FC<IEditor> = ({ mode }) => {
             return (
               <Form>
                 <FormSelect
-                  controlId="user-role"
+                  controlId="participant-role"
                   name="role_id"
                   options={roles}
                   inputGroupPrepend={<InputGroup.Text id="role-prepend">Role</InputGroup.Text>}
                 />
                 <FormInput
-                  controlId="user-name"
-                  label="Username"
+                  controlId="participant-name"
+                  label="Participantname"
                   name="name"
                   disabled={mode === "update"}
                   inputGroupPrepend={<InputGroup.Text id="user-name-prep">@</InputGroup.Text>}
@@ -129,18 +128,18 @@ const UserEditor: React.FC<IEditor> = ({ mode }) => {
                 <Row>
                   <FormInput
                     as={Col}
-                    controlId="user-first-name"
+                    controlId="participant-first-name"
                     label="First name"
                     name="firstName"
                   />
                   <FormInput
                     as={Col}
-                    controlId="user-last-name"
+                    controlId="participant-last-name"
                     label="Last name"
                     name="lastName"
                   />
                 </Row>
-                <FormInput controlId="user-email" label="Email" name="email" />
+                <FormInput controlId="participant-email" label="Email" name="email" />
                 <FormCheckBoxGroup
                   controlId="email-pref"
                   label="Email Preferences"
@@ -148,12 +147,12 @@ const UserEditor: React.FC<IEditor> = ({ mode }) => {
                   options={emailOptions}
                 />
                 <FormSelect
-                  controlId="user-institution"
+                  controlId="participant-institution"
                   name="institution_id"
-                  disabled={mode === "update" || auth.user.role !== ROLE.SUPER_ADMIN.valueOf()}
+                  disabled={mode === "update" || auth.participant.role !== ROLE.SUPER_ADMIN.valueOf()}
                   options={institutions}
                   inputGroupPrepend={
-                    <InputGroup.Text id="user-inst-prep">Institution</InputGroup.Text>
+                    <InputGroup.Text id="participant-inst-prep">Institution</InputGroup.Text>
                   }
                 />
                 <Modal.Footer>
@@ -166,7 +165,7 @@ const UserEditor: React.FC<IEditor> = ({ mode }) => {
                     type="submit"
                     disabled={!(formik.isValid && formik.dirty) || formik.isSubmitting}
                   >
-                    {mode === "update" ? "Update User" : "Create User"}
+                    {mode === "update" ? "Update Participant" : "Create Participant"}
                   </Button>
                 </Modal.Footer>
               </Form>
@@ -178,4 +177,4 @@ const UserEditor: React.FC<IEditor> = ({ mode }) => {
   );
 };
 
-export default UserEditor;
+export default ParticipantEditor;
