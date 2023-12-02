8f049f4d866912f0d64e47b179e3d6e874b862db
Shreya Vaidya <shreyakvaidya@gmail.com>
2023-12-02 15:24:11 -0500

    created separate file for static data

--- src/pages/Manage/Courses.jsx
+++ src/pages/Manage/Courses.jsx
@@ -15,41 +15,11 @@ import { useNavigate } from "react-router-dom";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CustomTooltip from "./utils/Tooltip";
 import EditModal from "./utils/Modal";
+import { staticTableData, staticInnerrowData } from "./utils/data";
 
 function Courses() {
-  const [tableData, setTableData] = useState([
-    [
-      "CSC/ECE 517, Fall 2023",
-      "North Carolina State University",
-      "Aug 31 2023 - 10:09 PM",
-      "Aug 31 2023 - 10:09 PM",
-    ],
-    [
-      "CSC/ECE 506, Summer 2023",
-      "North Carolina State University",
-      "May 23 2023 - 06:04 PM",
-      "May 23 2023 - 06:04 PM",
-    ],
-    [
-      "CSC/ECE 517, Spring 2023",
-      "North Carolina State University",
-      "Dec 29 2022 - 11:40 PM",
-      "Dec 29 2022 - 11:40 PM",
-    ],
-    [
-      "CSC/ECE 506, Spring 2023",
-      "North Carolina State University",
-      "Dec 29 2022 - 11:39 PM",
-      "Dec 29 2022 - 11:39 PM",
-    ],
-  ]);
-
-  const [innerrowData, setinnerrowData] = useState([
-    ["Final project (and design doc)", "-", "2023-11-10", "2023-11-10"],
-    ["Design exercise", "-", "2023-10-05", "2023-10-06"],
-    ["OSS Project and documentation", "-", "2023-09-30", "2023-09-30"],
-  ]);
-
+  const [tableData, setTableData] = useState(staticTableData);
+  const [innerrowData, setinnerrowData] = useState(staticInnerrowData);
   const [showModal, setShowModal] = useState(false);
   const [showInnerModal, setShowInnerModal] = useState(false);
   const [editRowData, setEditRowData] = useState({});
@@ -261,7 +231,7 @@ function Courses() {
     />,
   ];
 
-  const action = (rowData) => [
+  const action = (rowData, index) => [
     <CustomTooltip
       key="edit"
       title="Edit Course"
---
+++ src/pages/Manage/utils/data.js
@@ -0,0 +1,32 @@
+export const staticTableData = [
+  [
+    "CSC/ECE 517, Fall 2023",
+    "North Carolina State University",
+    "Aug 31 2023 - 10:09 PM",
+    "Aug 31 2023 - 10:09 PM",
+  ],
+  [
+    "CSC/ECE 506, Summer 2023",
+    "North Carolina State University",
+    "May 23 2023 - 06:04 PM",
+    "May 23 2023 - 06:04 PM",
+  ],
+  [
+    "CSC/ECE 517, Spring 2023",
+    "North Carolina State University",
+    "Dec 29 2022 - 11:40 PM",
+    "Dec 29 2022 - 11:40 PM",
+  ],
+  [
+    "CSC/ECE 506, Spring 2023",
+    "North Carolina State University",
+    "Dec 29 2022 - 11:39 PM",
+    "Dec 29 2022 - 11:39 PM",
+  ],
+];
+
+export const staticInnerrowData = [
+  ["Final project (and design doc)", "-", "2023-11-10", "2023-11-10"],
+  ["Design exercise", "-", "2023-10-05", "2023-10-06"],
+  ["OSS Project and documentation", "-", "2023-09-30", "2023-09-30"],
+];
