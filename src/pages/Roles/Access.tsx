f26213795f59da687b14e17764b1809f94197097
Shubh Nisar <snisar@ncsu.edu>
2024-03-18 16:08:39 -0400

    Role based access

---
+++ src/pages/Roles/Access.tsx
@@ -0,0 +1,7 @@
+export const access: Record<string, string[]> = {
+    "Student": ['Student'],
+    "Teaching Assistant": ["Teaching Assistant", "Student"],
+    "Instructor": ["Instructor", "Teaching Assistant", "Student"],
+    "Administrator": ["Administrator", "Instructor", "Teaching Assistant", "Student"],
+    "Super Administrator": ["Super Administrator", "Administrator", "Instructor", "Teaching Assistant", "Student"]
+};
