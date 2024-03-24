738b21778263294d3b5e22ec5c15b053c1d02561
hasini <hasinichenchala2@gmail.com>
2024-03-24 19:54:26 -0400

    The link to the StudentTask component was removed from the Home component to simplify the interface and improve user experience.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,10 @@
 /**
  * @author Ankur Mundra on May, 2023
  */
-import React from "react";
-import { Link } from "react-router-dom";
 const Home = () => {
-  const assignment_id = "5";
   return (
     <div>
-      <h1>Welcome Home!</h1>
-      <Link to={`/student_task_view?id=${assignment_id}`}>Go to Student Task</Link>
-      
+      <h1>Welcome Home!</h1>      
     </div>
   );
 };
