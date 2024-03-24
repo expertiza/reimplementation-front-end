04637b70ab068044bebe3b982a4139a93cf7407c
sinchanashetty11 <sinchz12@gmail.com>
2024-03-23 20:43:32 -0400

    refactoring code

--- src/pages/Roles/Access.tsx
+++ src/pages/Roles/Access.tsx
@@ -1,8 +1,22 @@
-// ROLE TO ACCESS LEVEL ARRAY
-export const access: Record<string, string[]> = {
-    "Student": ['Student'],
-    "Teaching Assistant": ["Teaching Assistant", "Student"],
-    "Instructor": ["Instructor", "Teaching Assistant", "Student"],
-    "Administrator": ["Administrator", "Instructor", "Teaching Assistant", "Student"],
-    "Super Administrator": ["Super Administrator", "Administrator", "Instructor", "Teaching Assistant", "Student"]
-};
+enum Role {
+    Student = "Student",
+    TeachingAssistant = "Teaching Assistant",
+    Instructor = "Instructor",
+    Administrator = "Administrator",
+    SuperAdministrator = "Super Administrator",
+  }
+  
+  export const access: Record<string, string[]> = {
+    [Role.Student]: [Role.Student],
+    [Role.TeachingAssistant]: [Role.TeachingAssistant, Role.Student],
+    [Role.Instructor]: [Role.Instructor, Role.TeachingAssistant, Role.Student],
+    [Role.Administrator]: [Role.Administrator, Role.Instructor, Role.TeachingAssistant, Role.Student],
+    [Role.SuperAdministrator]: [
+      Role.SuperAdministrator, 
+      Role.Administrator, 
+      Role.Instructor, 
+      Role.TeachingAssistant, 
+      Role.Student,
+    ],
+  };
+  
