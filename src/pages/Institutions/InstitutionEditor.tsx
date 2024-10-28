ca8dc08a458be8bd9ef41c1821114e63807b6742
Soubarnica Somangali Suresh <ssomang@ncsu.edu>
2024-10-28 19:44:58 -0400

    Fixed bug for close and update handles in InstitutionEditor.tsx to navigate to Manage Institutions page

--- src/pages/Institutions/InstitutionEditor.tsx
+++ src/pages/Institutions/InstitutionEditor.tsx
@@ -45,7 +45,7 @@ const InstitutionEditor: React.FC<IEditor> = ({ mode }) => {
           message: `Institution ${mode}d successfully!`,
         })
       );
-      navigate("/administrator/institutions");
+      navigate("/institutions");
     }
   }, [dispatch, mode, navigate, institutionResponse]);
 
@@ -71,7 +71,7 @@ const InstitutionEditor: React.FC<IEditor> = ({ mode }) => {
     submitProps.setSubmitting(false);
   };
 
-  const handleClose = () => navigate("/administrator/institutions");
+  const handleClose = () => navigate("/institutions");
 
   return (
     <Modal size="lg" centered show={true} onHide={handleClose} backdrop="static">
