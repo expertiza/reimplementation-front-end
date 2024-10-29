134af106f6d9104491e70a30e4898c17bb97172e
Pierce57 <piercewhelan806@gmail.com>
2024-10-29 04:41:12 -0400

    Putting the Impersonate Button Spinner into a separate CSS file

--- src/pages/Impersonate/ImpersonateUser.tsx
+++ src/pages/Impersonate/ImpersonateUser.tsx
@@ -5,11 +5,11 @@ import debounce from "lodash.debounce";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../store/store";
 import { alertActions } from "store/slices/alertSlice";
-import { ILoggedInUser } from "../../utils/interfaces";
 import { authenticationActions } from "../../store/slices/authenticationSlice";
 import { useLocation, useNavigate } from "react-router-dom";
 import { setAuthToken } from "../../utils/auth";
 import masqueradeMask from "../../assets/masquerade-mask.png";
+import "./ImpersonateUser.css";
 
 const ImpersonateUser: React.FC = () => {
   const { data: userResponse, sendRequest: fetchUsers } = useAPI();
@@ -24,7 +24,6 @@ const ImpersonateUser: React.FC = () => {
   const [selectedValidUser, setSelectedValidUser] = useState(false);
   const [impersonateActive, setImpersonateActive] = useState(false);
   const [typingTimeout, setTypingTimeout] = useState<NodeJS.Timeout | null>(null);
-  // const [originalToken, setOriginalToken] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const location = useLocation();
@@ -255,40 +254,8 @@ const ImpersonateUser: React.FC = () => {
           {displayUserList()}
         </div>
       </div>
-      <style>
-        {`
-          .loader {
-            width: 32px;
-            aspect-ratio: 1;
-            display: grid;
-            border-radius: 50%;
-            background:
-              linear-gradient(0deg ,rgb(0 0 0/50%) 30%,#0000 0 70%,rgb(0 0 0/100%) 0) 50%/8% 100%,
-              linear-gradient(90deg,rgb(0 0 0/25%) 30%,#0000 0 70%,rgb(0 0 0/75% ) 0) 50%/100% 8%;
-            background-repeat: no-repeat;
-            animation: l23 1s infinite steps(12);
-          }
-          .loader::before,
-          .loader::after {
-            content: "";
-            grid-area: 1/1;
-            border-radius: 50%;
-            background: inherit;
-            opacity: 0.915;
-            transform: rotate(30deg);
-          }
-          .loader::after {
-            opacity: 0.83;
-            transform: rotate(60deg);
-          }
-          @keyframes l23 {
-            100% {transform: rotate(1turn)}
-          }
-        `}
-      </style>
     </>
   );
 };
 
 export default ImpersonateUser;
-
