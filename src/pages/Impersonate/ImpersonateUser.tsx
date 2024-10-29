bd68c7f885581450d54d8ab160ea7e2aa6886cd6
mochi6 <Hechunz133@gmail.com>
2024-10-29 02:32:20 -0400

    "Changed Impersonate Spinner implementation"

--- src/pages/Impersonate/ImpersonateUser.tsx
+++ src/pages/Impersonate/ImpersonateUser.tsx
@@ -19,6 +19,7 @@ const ImpersonateUser: React.FC = () => {
   const [debounceActive, setDebounceActive] = useState(false);
   const [selectedValidUser, setSelectedValidUser] = useState(false);
   const [impersonateActive, setImpersonateActive] = useState(false);
+  const [typingTimeout, setTypingTimeout] = useState<NodeJS.Timeout | null>(null);
   // const [originalToken, setOriginalToken] = useState("");
   const dispatch = useDispatch();
 
@@ -32,8 +33,16 @@ const ImpersonateUser: React.FC = () => {
 
   // Handle search query input change and trigger debounce
   const handleSearchQueryInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (typingTimeout) clearTimeout(typingTimeout);
+
     setSearchQuery(e.target.value);
     setDebounceActive(true);
+
+    const timeout = setTimeout(() => {
+      setDebounceActive(false);
+    }, 300);
+
+    setTypingTimeout(timeout);
   };
 
   // Debounce search query
@@ -44,10 +53,10 @@ const ImpersonateUser: React.FC = () => {
   // Cleanup debounce function when component unmounts
   useEffect(() => {
     return () => {
+      if (typingTimeout) clearTimeout(typingTimeout);
       debouncedSearch.cancel();
-      setDebounceActive(false);
     };
-  }, [debouncedSearch]);
+  }, [debouncedSearch, typingTimeout]);
 
   // Display user list after debounce (autocomplete functionality)
   const displayUserList = () => {
@@ -61,7 +70,7 @@ const ImpersonateUser: React.FC = () => {
       user?.name?.toLowerCase().includes(searchQuery.toLowerCase())
     );
 
-    if (debounceActive && filteredUserArray.length > 0) {
+    if (filteredUserArray.length > 0) {
       return (
         <Dropdown show>
           <Dropdown.Menu>
@@ -205,6 +214,7 @@ const ImpersonateUser: React.FC = () => {
               value={searchQuery}
               onChange={handleSearchQueryInput}
             />
+            
             <Button
               variant="outline-secondary"
               id="button-addon2"
@@ -213,12 +223,45 @@ const ImpersonateUser: React.FC = () => {
             >
               Impersonate
             </Button>
+            {debounceActive && <div className="loader"></div>}
           </InputGroup>
           {displayUserList()}
         </div>
       </div>
+      <style>
+        {`
+          .loader {
+            width: 50px;
+            aspect-ratio: 1;
+            display: grid;
+            border-radius: 50%;
+            background:
+              linear-gradient(0deg ,rgb(0 0 0/50%) 30%,#0000 0 70%,rgb(0 0 0/100%) 0) 50%/8% 100%,
+              linear-gradient(90deg,rgb(0 0 0/25%) 30%,#0000 0 70%,rgb(0 0 0/75% ) 0) 50%/100% 8%;
+            background-repeat: no-repeat;
+            animation: l23 1s infinite steps(12);
+          }
+          .loader::before,
+          .loader::after {
+            content: "";
+            grid-area: 1/1;
+            border-radius: 50%;
+            background: inherit;
+            opacity: 0.915;
+            transform: rotate(30deg);
+          }
+          .loader::after {
+            opacity: 0.83;
+            transform: rotate(60deg);
+          }
+          @keyframes l23 {
+            100% {transform: rotate(1turn)}
+          }
+        `}
+      </style>
     </>
   );
 };
 
 export default ImpersonateUser;
+
