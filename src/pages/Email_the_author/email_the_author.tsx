35db9ac09e8e6c59c67ebe38549eb7fea1312698
ishani-rajput <rajputishani515@gmail.com>
2025-03-06 04:42:53 -0600

    Email message internationalization

--- src/pages/Email_the_author/email_the_author.tsx
+++ src/pages/Email_the_author/email_the_author.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import "./email_the_author.css";
+import { useTranslation } from "react-i18next"; // Importing useTranslation hook
+
 const Email_the_author: React.FC = () => {
+  const { t } = useTranslation(); // Initialize useTranslation hook
+
   return (
     <div id="div1">
-      <h2>Email the author</h2>
+      <h2>{t('email_the_author.title')}</h2>
       <div>
-        <label htmlFor="subject">Subject:</label>
+        <label htmlFor="subject">{t('email_the_author.subject')}</label>
         <input type="text" id="subject" name="subject" />
       </div>
       <div>
-        <label htmlFor="description">Description:</label>
-        <textarea id="description" name="description" ></textarea>
+        <label htmlFor="description">{t('email_the_author.description')}</label>
+        <textarea id="description" name="description"></textarea>
       </div>
-      <button id="b1">Send</button>
+      <button id="b1">{t('email_the_author.send')}</button>
     </div>
   );
 };
