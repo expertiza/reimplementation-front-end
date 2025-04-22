9344fbd2d66e32c90542e77564329cbbe7940198
Maya Mei <gmei@ncsu.edu>
2025-04-22 14:15:51 -0400

    fixed automatic updating of table

--- src/pages/AssignmentParticipants/AssignmentParticipants.tsx
+++ src/pages/AssignmentParticipants/AssignmentParticipants.tsx
@@ -26,10 +26,10 @@ function AssignmentParticipants({ assignmentProps }: AssignmentParticipantsProps
   const { data: usersResponse, sendRequest: fetchUsers } = useAPI();
   const { data: assignmentResponse, sendRequest: fetchAssignment } = useAPI();
 
-  const { sendRequest: addParticipant } = useAPI();
-  const { sendRequest: updateParticipant } = useAPI();  
-  const { sendRequest: updateUser } = useAPI();  
-  const { sendRequest: deleteParticipant } = useAPI();
+  const { data: addParticipantResponse, sendRequest: addParticipant } = useAPI();
+  const { data: updateParticipantResponse, sendRequest: updateParticipant } = useAPI();  
+  const { data: updateUserResponse, sendRequest: updateUser } = useAPI();  
+  const { data: deleteParticipantResponse, sendRequest: deleteParticipant } = useAPI();
 
   const [participants, setParticipants] = useState<Participant[]>([]);
   const [newUserName, setNewUserName] = useState<string>('');
@@ -55,13 +55,10 @@ function AssignmentParticipants({ assignmentProps }: AssignmentParticipantsProps
   }, [
     modalShow,
     assignmentId,
-    fetchParticipants,
-    fetchUsers,
-    fetchAssignment,
-    addParticipant,
-    updateParticipant,
-    updateUser,
-    deleteParticipant,
+    addParticipantResponse,
+    updateParticipantResponse,
+    updateUserResponse,
+    deleteParticipantResponse,
   ]);
 
   //setting participants state
