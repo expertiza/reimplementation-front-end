c5524c8af6e8d2acd921fe1b848248c91e939e07
SiddhiKhairee <siddhi.k612@gmail.com>
2025-10-29 13:55:55 -0400

    change

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', function (req, res) {
+// Handle React routing - return all requests to React app
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
